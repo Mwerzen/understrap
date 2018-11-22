7129fed8f4480e9f5907cae7d3018dfaf3f874ac
mwerzen <mwerzen@gmail.com>
2018-11-22 03:20:35 -0500

    Updated Color Scheme

--- header.php
+++ header.php
@@ -42,7 +42,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
 
-		<nav class="navbar navbar-expand-md navbar-dark color-primary-light shadow">
+		<nav class="navbar navbar-expand-md navbar-dark color-gradient shadow">
 
 		<?php if ( 'container' == $container ) : ?>
 			<div class="container" >
